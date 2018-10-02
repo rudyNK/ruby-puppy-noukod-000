916f7f0566a8b658b7eeeb3353ba67a5d46faca4
Rudy Ravelin <rudy@noukod.com>
2018-10-02 20:00:32 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Puppy
+  def initialize(name)
+  	@name = name
+  end
+	
+  def bark
+  	puts "#{@name} says: Woof!"
+  end
+end
+
+# we make an array of names to build puppies with
+names = ["Fido", "Spot", "Duchess", "Ginger"]
+
+# we make an empty container for our puppy collection
+puppies = []
+
+puts "Iterating through names list to create puppies ..."
+names.each do |name|
+  puts "Creating a puppy named #{name} ..."
+  puppies << Puppy.new(name)
+  puts "There are now #{puppies.length} Puppy instances in the array"
+  puts "----"
+end
+
+puts "Testing each Puppy instance in the array to make sure it can bark ..."
+puppies.each do |puppy|
+  puppy.bark
+end
